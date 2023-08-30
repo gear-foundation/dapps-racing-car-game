b193f52d323393955a9f740f7e81a3596e0f3a39
Eugene Kotsarev <gimmethenick@gmail.com>
2023-08-30 21:21:16 +0300

    upd welcome text and add max gas limit to messages (#2)

--- frontend/src/features/Auth/components/Welcome/Welcome.tsx
+++ frontend/src/features/Auth/components/Welcome/Welcome.tsx
@@ -6,10 +6,15 @@ function Welcome({ children }: WelcomeProps) {
   return (
     <div className={cx(styles.content)}>
       <h1 className={cx(styles['main-title'], styles['main-title-with-gradient'])}>Racing Car Game</h1>
-      <p className={cx(styles['main-description'])}>
-        A racing car game in which you compete not against a human, but against a smart contract. You will be given the
-        choice to either accelerate or shoot at the nearest car.
-      </p>
+      <div className={cx(styles['main-description'])}>
+       <p>
+         A racing car game in which you compete not against a human, but against a smart contract. You will be given the
+         choice to either accelerate or shoot at the nearest car.
+       </p>
+        <p>
+          To play the game you need to have at least 250 TVARA on your balance.
+        </p>
+      </div>
 
       {children}
     </div>
