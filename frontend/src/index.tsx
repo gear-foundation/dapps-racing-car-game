c1fe9e87e0a35838b197cde78aee97172ec8d174
ereburg <gimmethenick@gmail.com>
2023-09-11 13:49:20 +0300

    add GTM analytics module

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client';
+import TagManager from 'react-gtm-module';
 import { App } from '@/App';
-
 import './styles/global.scss';
+
+if (process.env.NODE_ENV === 'production') {
+  TagManager.initialize({
+    gtmId: 'GTM-5H6FVXRQ',
+  });
+}
 
 const container = document.getElementById('root');
 const root = createRoot(container as Element);
